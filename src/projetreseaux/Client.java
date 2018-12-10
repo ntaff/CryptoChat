cafb4b6d050e888f25e81362dfbc78258236b662
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2018-12-10 14:00:39 +0100

    Update Client.java

--- src/projetreseaux/Client.java
+++ src/projetreseaux/Client.java
@@ -1,48 +1,56 @@
-package projetreseaux;
+package chatavecchiffrage;
 
 import java.io.*;
 import java.net.*;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
 
 
 public class Client {
-   static final int port = 4444;
-   
-
-
-   public static void main(String[] args) throws Exception {
-        boolean isReading = true;
-        Socket socket = new Socket("localhost", port);
-
-        BufferedReader plec = new BufferedReader(
-                       new InputStreamReader(socket.getInputStream())
-                       );
-
-                
-        PrintWriter pred = new PrintWriter(
-                             new BufferedWriter(
-                                new OutputStreamWriter(socket.getOutputStream())),
-                             true);
-       
-        
-        BufferedReader input = new BufferedReader(
-                       new InputStreamReader(System.in)
-                       );
-
     
-        String str = "";
-        while(!str.equals("bye")){
-            if(isReading){
-                str = plec.readLine();
-                System.out.println(str);
-            }else{
-                str = input.readLine();
-                pred.println(str);
-            }
-            isReading = !isReading;
-        }
+   public static void main(String[] args) throws Exception {
+        boolean isReading = !Serveur.isReading;
 
-        plec.close();
-        pred.close();
-        socket.close();
+        try (Socket client = new Socket("localhost", Serveur.PORT);
+            DataInputStream clientIn = new DataInputStream(client.getInputStream());
+            DataOutputStream clientOut = new DataOutputStream(client.getOutputStream());
+            BufferedReader messageInput = new BufferedReader(new InputStreamReader(System.in))) {
+           
+           SecretKey key = CleAES.importKey(Serveur.KEY_FILE);
+           Cipher cipher = Cipher.getInstance("AES");
+           
+           boolean isStopped = false;
+           String message = "";
+           while (!isStopped && !message.equals("bye")) {
+               if(isReading){
+                   cipher.init(Cipher.DECRYPT_MODE, key);
+                   
+                   int messageSize = clientIn.readInt();
+                   byte[] encryptedMessage = new byte[messageSize];
+                   
+                   clientIn.readFully(encryptedMessage);
+                   
+                   byte[] decryptedMessage = cipher.doFinal(encryptedMessage);
+                   
+                   System.out.println(new String(encryptedMessage)+" -> "+new String(decryptedMessage));
+                   
+                   isStopped = new String(decryptedMessage).equals("bye");
+               }else{
+                   cipher.init(Cipher.ENCRYPT_MODE, key);
+                   
+                   message = messageInput.readLine();
+                   
+                   byte[] encryptedMessage = cipher.doFinal(message.getBytes());
+                   
+                   System.out.println(message+" -> "+new String(encryptedMessage));
+                   
+                   clientOut.writeInt(encryptedMessage.length);
+                   clientOut.write(encryptedMessage);
+               }
+               isReading = !isReading;
+           }
+           
+       }
    }
+   
 }
