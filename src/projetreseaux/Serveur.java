fdb1a326508d3f24959f446072c6728bae025b9f
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2018-12-10 14:00:21 +0100

    Update Serveur.java

--- src/projetreseaux/Serveur.java
+++ src/projetreseaux/Serveur.java
@@ -1,46 +1,62 @@
-package projetreseaux;
+package chatavecchiffrage;
 
 import java.io.*;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
 
 public class Serveur {
-   static final int port = 4444;
+   static final int PORT = 4444; 
+   static final String KEY_FILE = "cle.txt";
+   static boolean isReading = false;
    
-
    public static void main(String[] args) throws Exception {
-        boolean isReading = false;
-        ServerSocket serveur = new ServerSocket(port);
-        Socket client = serveur.accept();
-
-        BufferedReader plec = new BufferedReader(
-                               new InputStreamReader(client.getInputStream())
-                              );
-
-        PrintWriter pred = new PrintWriter(
-                             new BufferedWriter(
-                                new OutputStreamWriter(client.getOutputStream())), 
-                             true);
-        
-        BufferedReader input = new BufferedReader(
-                               new InputStreamReader(System.in)
-                              );
+        SecretKey key = CleAES.generateKey();
+        CleAES.exportKey(key,KEY_FILE);
+       
+        try(ServerSocket serveur = new ServerSocket(PORT);
+            Socket client = serveur.accept();
+            DataInputStream clientIn = new DataInputStream(client.getInputStream());
+            DataOutputStream clientOut = new DataOutputStream(client.getOutputStream());
+            BufferedReader messageInput = new BufferedReader(new InputStreamReader(System.in))){
         
         
-        System.out.println("connexion de "+client.getInetAddress().toString()+" sur le port "+client.getPort());
+        System.out.println("Connexion de "+client.getInetAddress()+" sur le port "+client.getLocalPort());
         
-        String str = "";
-        while (!str.equals("bye")) {
+        Cipher cipher = Cipher.getInstance("AES");
+        
+        boolean isStopped = false;
+        String message = "";
+        while (!isStopped && !message.equals("bye")) {
             if(isReading){
-                str = plec.readLine();
-                System.out.println(str);
+                cipher.init(Cipher.DECRYPT_MODE, key);
+
+                int messageSize = clientIn.readInt();
+                byte[] encryptedMessage = new byte[messageSize];
+
+                clientIn.readFully(encryptedMessage);
+
+                byte[] decryptedMessage = cipher.doFinal(encryptedMessage);
+
+                System.out.println(new String(encryptedMessage)+" -> "+new String(decryptedMessage));
+
+                isStopped = new String(decryptedMessage).equals("bye");
             }else{
-                str = input.readLine();
-                pred.println(str);
+                cipher.init(Cipher.ENCRYPT_MODE, key);
+
+                message = messageInput.readLine();
+
+                byte[] encryptedMessage = cipher.doFinal(message.getBytes());
+
+                System.out.println(message+" -> "+new String(encryptedMessage));
+
+                clientOut.writeInt(encryptedMessage.length);
+                clientOut.write(encryptedMessage);
             }
             isReading = !isReading;
         }
-        plec.close();
-        pred.close();
-        client.close();
+        
+      }    
    }
 }
