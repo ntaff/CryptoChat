62602662b89e12909d8b669dd0e3eb605664ffc0
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2018-12-10 14:01:03 +0100

    Update CleAES.java

--- src/projetreseaux/CleAES.java
+++ src/projetreseaux/CleAES.java
@@ -1,43 +1,52 @@
-package projetreseaux;
+package chatavecchiffrage;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class CleAES {
     
-    public void exportKey(SecretKey key) throws IOException{
-        BufferedWriter writer = new BufferedWriter(new FileWriter("cle2.txt"));
-        writer.write(Arrays.toString(key.getEncoded()));
-        writer.close();
-    }
-    
-    public SecretKey generateKey(){
-        KeyGenerator keyGen;
+    public static SecretKey importKey(String inputFile) throws Exception{
+        byte[] byteKey = Files.readAllBytes(Paths.get(inputFile));
         
-        try {
-          keyGen = KeyGenerator.getInstance("AES");
-          keyGen.init(128);
-          
-          SecretKey cle = keyGen.generateKey();
-          
-          return cle;
-
-        } catch (Exception e) {
-          e.printStackTrace();
-          return null;
+        SecretKey importedKey;
+        
+        try (FileInputStream keyIn = new FileInputStream(inputFile)) {
+            importedKey = new SecretKeySpec(byteKey, "AES");
         }
-
+        
+        return importedKey;
     }
     
-    public static void main(String[] args) throws IOException {
-        CleAES aes = new CleAES();
-        SecretKey cle = aes.generateKey();
-        System.out.println(cle);
-        aes.exportKey(cle);
+    public static void exportKey(SecretKey key, String outputFile) throws Exception{
+        try (FileOutputStream keyOut = new FileOutputStream(outputFile)) {
+            keyOut.write(key.getEncoded());
+        }
+    }
+    
+    public static SecretKey generateKey() throws NoSuchAlgorithmException{       
+          KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+          keyGen.init(256);
+          
+          SecretKey key = keyGen.generateKey();
 
+          return key;
+    }
+    
+    public static void main(String[] args) throws IOException, Exception {
+        SecretKey generatedKey = CleAES.generateKey();
+        
+        CleAES.exportKey(generatedKey,"cle.txt");
+        
+        SecretKey importedKey = CleAES.importKey("cle.txt");
+        
+        System.out.println(Arrays.equals(generatedKey.getEncoded(), importedKey.getEncoded()));
   }
 }
